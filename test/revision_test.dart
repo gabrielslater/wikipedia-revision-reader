9f02dc1cebe5e66d1aca85c64d6d2e86493978e9
gabriel slater <gabrielxslater@gmail.com>
2022-09-17 15:27:15 -0400

    feat: add function to read json from file

--- test/revision_test.dart
+++ test/revision_test.dart
@@ -1,2 +1,16 @@
-void main() {
+import 'dart:io';
+
+import 'package:path/path.dart' as p;
+
+void main() async {}
+
+/// Reads a JSON file from test files.
+/// [filename] does not include extension
+Future<String> readJson(String filename) async {
+  // written with assistance of
+  // https://docs.flutter.dev/cookbook/persistence/reading-writing-files#4-read-data-from-the-file
+  final file = File(p.join('test', 'data', '$filename.json'));
+  final contents = await file.readAsString();
+
+  return contents;
 }
