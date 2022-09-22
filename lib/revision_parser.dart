51cc4cdacbdcda42b802757069bfd6c34cdf512d
gabriel slater <gabrielxslater@gmail.com>
2022-09-22 18:03:28 -0400

    feat: make parserevisions return string revisions

--- lib/revision_parser.dart
+++ lib/revision_parser.dart
@@ -1,5 +1,8 @@
 class RevisionParser {
   List<String> parseRevisions(Map<String, dynamic> json) {
-    return <String>[];
+    var revisions = json['query']['pages'][0]['revisions'];
+    // https://stackoverflow.com/questions/60105956/how-to-cast-dynamic-to-liststring#60106251
+    // Map each item in the "revisions" to a string for now.
+    return (revisions as List).map((item) => item.toString()).toList();
   }
 }
