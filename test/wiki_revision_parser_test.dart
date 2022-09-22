842d650cfa0bf17cc40376dbf745aa6efd7b954d
gabriel slater <gabrielxslater@gmail.com>
2022-09-22 15:53:01 -0400

    test: create revisionparser
    RevisionParser isn't static. This test should have been written before 1e87a04fd2c5cd036d4bbd24d886bb586e4306f4.

--- test/wiki_revision_parser_test.dart
+++ test/wiki_revision_parser_test.dart
@@ -1,13 +1,23 @@
 import 'dart:convert';
 
+import 'package:test/test.dart';
 import 'package:twp_payton_h_gabriel_s/read_json.dart';
+import 'package:twp_payton_h_gabriel_s/revision_parser.dart';
 
 void main() async {
   final queries = ['c._elegans', 'pet_door', 'jack_brierley'];
   var revisions = {};
 
+  final revisionParser = RevisionParser();
+
   for (String query in queries) {
     var json = jsonDecode(await readJson(query));
     revisions[query] = RevisionParser.parseRevisions(json);
   }
+
+  group('revision_parser', () {
+    test('RevisionParser created successfully', () {
+      expect(revisionParser.runtimeType, RevisionParser);
+    });
+  });
 }
