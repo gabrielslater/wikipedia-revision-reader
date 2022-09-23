92c789c8b6c286db82be52909aec2f61cda61a67
gabriel slater <gabrielxslater@gmail.com>
2022-09-23 16:26:57 -0400

    feat: assign isanon based on json 'anon' property

--- lib/wiki_revision.dart
+++ lib/wiki_revision.dart
@@ -1,7 +1,10 @@
 class WikiRevision {
   final String user;
+  final bool isAnon;
 
-  WikiRevision(this.user);
+  WikiRevision(this.user, this.isAnon);
 
-  WikiRevision.fromJson(Map<String, dynamic> json) : user = json['user'];
+  WikiRevision.fromJson(Map<String, dynamic> json)
+      : user = json['user'],
+        isAnon = (json['anon'] != null ? true : false);
 }
