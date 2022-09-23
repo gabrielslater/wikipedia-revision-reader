d438741c0fe2418eee9287290eb40aa41796d1f4
gabriel slater <gabrielxslater@gmail.com>
2022-09-23 16:38:26 -0400

    feat: parse datetime from timestamp property

--- lib/wiki_revision.dart
+++ lib/wiki_revision.dart
@@ -1,10 +1,12 @@
 class WikiRevision {
   final String user;
   final bool isAnon;
+  final DateTime timestamp;
 
-  WikiRevision(this.user, this.isAnon);
+  WikiRevision(this.user, this.isAnon, this.timestamp);
 
   WikiRevision.fromJson(Map<String, dynamic> json)
       : user = json['user'],
-        isAnon = (json['anon'] != null ? true : false);
+        isAnon = (json['anon'] != null ? true : false),
+        timestamp = DateTime.parse(json['timestamp']);
 }
