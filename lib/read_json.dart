0eb79a6ad01ceecea8f1bbd0a759459ea152a2bd
gabriel slater <gabrielxslater@gmail.com>
2022-09-24 19:52:00 -0400

    feat: change readjson's returned object to map<string, dynamic>

--- lib/read_json.dart
+++ lib/read_json.dart
@@ -1,14 +1,15 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:path/path.dart' as p;
 
 /// Reads a JSON file from test files.
 /// [filename] does not include extension
-Future<String> readJson(String filename) async {
+Future<Map<String, dynamic>> readJson(String filename) async {
   // written with assistance of
   // https://docs.flutter.dev/cookbook/persistence/reading-writing-files
   final file = File(p.join('test', 'data', '$filename.json'));
   final contents = await file.readAsString();
 
-  return contents;
+  return jsonDecode(contents);
 }
