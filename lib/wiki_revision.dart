cf4abe358c3a9f5eb41454be6ae890742565b178
gabriel slater <gabrielxslater@gmail.com>
2022-09-25 16:24:20 -0400

    feat: remove 'userhidden' check from isanon
    A user that is hidden is technically anonymous but doesn't have the other necessary parts (IP address) to be anonymous in the ListView.

--- lib/wiki_revision.dart
+++ lib/wiki_revision.dart
@@ -6,8 +6,7 @@ class WikiRevision {
   WikiRevision(this.user, this.isAnon, this.timestamp);
 
   WikiRevision.fromJson(Map<String, dynamic> json)
-      : user = (json['userhidden'] != null ? '' : json['user']),
-        isAnon =
-            (json['anon'] != null || json['userhidden'] != null ? true : false),
+      : user = (json['userhidden'] != null ? 'User hidden' : json['user']),
+        isAnon = (json['anon'] != null ? true : false),
         timestamp = DateTime.parse(json['timestamp']);
 }
