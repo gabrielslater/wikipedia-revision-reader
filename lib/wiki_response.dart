5d4348d45ef326a6e6cf88e3d0224d1ba0054f19
gabriel slater <gabrielxslater@gmail.com>
2022-09-25 15:50:07 -0400

    feat: return an empty wikiresponse if no page is found
    The UI shouldn't be handling errors.

--- lib/wiki_response.dart
+++ lib/wiki_response.dart
@@ -12,17 +12,20 @@ class WikiResponse {
   factory WikiResponse.fromJson(Map<String, dynamic> json) {
     var pageId = json['query']['pages'].keys.toList().first;
     var page = json['query']['pages'][pageId];
-
-    return WikiResponse(
-        page['title'],
-        // https://stackoverflow.com/questions/60105956/how-to-cast-dynamic-to-liststring#60106251
-        (page['revisions'] as List)
-            .map((item) => WikiRevision.fromJson(item))
-            .toList(),
-        // If there is no redirect, return an empty map, otherwise return the
-        // redirect map.
-        (json['query']['redirects'] != null)
-            ? json['query']['redirects'][0]
-            : {});
+    if (pageId == '-1') {
+      return WikiResponse('', [], {});
+    } else {
+      return WikiResponse(
+          page['title'],
+          // https://stackoverflow.com/questions/60105956/how-to-cast-dynamic-to-liststring#60106251
+          (page['revisions'] as List)
+              .map((item) => WikiRevision.fromJson(item))
+              .toList(),
+          // If there is no redirect, return an empty map, otherwise return the
+          // redirect map.
+          (json['query']['redirects'] != null)
+              ? json['query']['redirects'][0]
+              : {});
+    }
   }
 }
