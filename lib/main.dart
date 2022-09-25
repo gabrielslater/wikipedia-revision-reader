96ae1918666453f248f08a572f9abc59518557f9
gabriel slater <gabrielxslater@gmail.com>
2022-09-24 23:29:45 -0400

    feat: attach back end to front end

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:twp_payton_h_gabriel_s/wiki_requester.dart';
+import 'package:twp_payton_h_gabriel_s/wiki_revision.dart';
 
 void main() {
   runApp(const WikipediaRevisionReader());
@@ -32,39 +36,77 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   final _controller = TextEditingController();
+  final _wikiRequester = WikiRequester();
+
+  /// necessary to prevent empty space when the app opens
+  bool _hasRequestedOnce = false;
+  bool _isProcessing = false;
+  bool _isRedirect = false;
+  bool _isError = false;
+
+  var _errorText = '';
+  var _pageTitle = '';
+  var _redirectText = '';
+  var _revisionsList = <WikiRevision>[];
 
   @override
   Widget build(BuildContext context) {
-    Widget pageTitle = Container(
-      padding: const EdgeInsets.all(32),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text(
-              'Page Title',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Text(
-            'Redirected from',
-            style: TextStyle(
-                color: Theme.of(context).colorScheme.secondary,
-                fontSize: 20,
-                fontStyle: FontStyle.italic),
-          )
-        ],
+    var redirect = Text(
+      _redirectText,
+      style: TextStyle(
+          color: Theme.of(context).colorScheme.secondary,
+          fontSize: 20,
+          fontStyle: FontStyle.italic),
+    );
+
+    var title = Text(
+      _pageTitle,
+      style: const TextStyle(
+        fontSize: 40,
+        fontWeight: FontWeight.bold,
       ),
+    );
+
+    Widget errorMessage = Container(
+      child: (_isError
+          ? Text(
+              _errorText,
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.error,
+                fontSize: 30,
+              ),
+            )
+          : null),
+    );
+
+    Widget pageTitle = Container(
+      child: (!_isError
+          ? Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: (_hasRequestedOnce && !_isProcessing ? title : null),
+                ),
+                Container(
+                  child: (_isRedirect ? redirect : null),
+                )
+              ],
+            )
+          : null),
     );
 
     Widget searchForm = Column(
       children: [
-        TextFormField(controller: _controller),
+        TextFormField(
+          controller: _controller,
+          enabled: !_isProcessing,
+        ),
         Container(
           padding: const EdgeInsets.all(8),
-          child: const ElevatedButton(
-              onPressed: null, child: Text('Get revisions')),
+          child: ElevatedButton(
+              onPressed: (!_isProcessing ? _createWikiResponse : null),
+              child: const Text('Get revisions')),
         ),
       ],
     );
@@ -72,17 +114,22 @@ class _MainPageState extends State<MainPage> {
     var revisionList = Expanded(
       child: Container(
         padding: const EdgeInsets.all(40),
-        child: ListView.builder(
-          scrollDirection: Axis.vertical,
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(8),
-          itemCount: 30,
-          clipBehavior: Clip.hardEdge,
-          itemBuilder: (BuildContext context, int index) {
-            return _buildRevisionItem(
-                Theme.of(context).colorScheme, index % 2 == 0);
-          },
-        ),
+        child: (_revisionsList.isNotEmpty
+            ? ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(8),
+                itemCount: _revisionsList.length,
+                clipBehavior: Clip.hardEdge,
+                itemBuilder: (BuildContext context, int index) {
+                  return _buildRevisionItem(
+                    Theme.of(context).colorScheme,
+                    index % 2 == 0,
+                    _revisionsList[index],
+                  );
+                },
+              )
+            : null),
       ),
     );
 
@@ -97,6 +144,7 @@ class _MainPageState extends State<MainPage> {
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              errorMessage,
               pageTitle,
               searchForm,
               revisionList,
@@ -107,26 +155,80 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  Container _buildRevisionItem(ColorScheme colorScheme, bool isEven) {
+  Future<void> _createWikiResponse() async {
+    var query = _controller.value.text;
+
+    if (query.isNotEmpty) {
+      if (!_hasRequestedOnce) {
+        _hasRequestedOnce = true;
+      }
+
+      try {
+        setState(() {
+          _isProcessing = true;
+        });
+
+        var response = await _wikiRequester.fetchPage(query);
+
+        setState(() {
+          _isProcessing = false;
+          _isError = false;
+          _pageTitle = response.title;
+          _isRedirect = response.redirect.isNotEmpty;
+
+          if (_isRedirect) {
+            _redirectText = 'Redirected from "${response.redirect['from']}"';
+          }
+
+          _revisionsList = response.revisions;
+        });
+      } on SocketException {
+        setState(() {
+          _isProcessing = false;
+          _isError = true;
+
+          _errorText = 'Cannot reach Wikipedia. Do you have a connection?';
+          _revisionsList = [];
+          _isRedirect = false;
+        });
+      } catch (e) {
+        setState(() {
+          _isProcessing = false;
+          _isError = true;
+
+          _errorText = 'No page found with name "$query"';
+          _revisionsList = [];
+          _isRedirect = false;
+        });
+      }
+    }
+  }
+
+  Container _buildRevisionItem(
+      ColorScheme colorScheme, bool isEven, WikiRevision revision) {
     return Container(
       color: (isEven ? colorScheme.surface : colorScheme.primaryContainer),
       padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
       child: Row(
         children: <Widget>[
           Column(
-            children: const <Widget>[
+            children: <Widget>[
               Text(
-                'User',
-                style: TextStyle(fontSize: 25),
+                revision.isAnon ? 'Anonymous' : revision.user,
+                style: const TextStyle(fontSize: 25),
               ),
-              Text(
-                '(IP address)',
-                style: TextStyle(fontStyle: FontStyle.italic),
+              Container(
+                child: (revision.isAnon
+                    ? Text(
+                        revision.user,
+                        style: const TextStyle(fontStyle: FontStyle.italic),
+                      )
+                    : null),
               ),
             ],
           ),
           const Spacer(),
-          const Text('Timestamp')
+          Text(revision.timestamp.toString())
         ],
       ),
     );
